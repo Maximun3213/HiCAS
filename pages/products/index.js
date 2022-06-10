93e35334c7c3e0e0c3d77198129ebeec05459556
Đạt  Huỳnh <datpanda3@gmail.com>
2022-06-10 10:26:58 +0700

    Hoàn thiện ngôn ngữ trang sản phẩm chính

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment } from "react";
 import classNames from "classnames/bind";
 import Image from "next/image";
 import Link from "next/link";
@@ -7,25 +7,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/router";
 
 import TitleSection from "../../src/components/titleSection";
+import Header from "../../src/components/header";
+import Footer from "../../src/components/footer";
 import styles from "./products.module.scss";
 const cx = classNames.bind(styles);
 
-function Products() {
+function Products({ header, product, footer }) {
   const router = useRouter();
-  const [locale, setLocale] = useState(router.locale);
-  const [products, setProducts] = useState([]);
   const API_URL = process.env.API_URL;
-
-  useEffect(() => {
-    fetch(`${API_URL}/products?_locale=` + locale)
-      .then((res) => res.json())
-      .then((product) => {
-        setProducts(product);
-      });
-  }, [locale]);
 
   return (
     <Fragment>
+      <Header header={header} />
       {router.locale === "en" ? (
         <TitleSection>Typical products</TitleSection>
       ) : (
@@ -34,7 +27,7 @@ function Products() {
 
       <div className={cx("container")}>
         <div className={cx("wapper")}>
-          {products.map((value) => (
+          {product.map((value) => (
             <div className={cx("card")} key={value.id}>
               <div className={cx("img_wapper")}>
                 <Image
@@ -63,8 +56,55 @@ function Products() {
           ))}
         </div>
       </div>
+      <Footer footer={footer} />
     </Fragment>
   );
 }
 
 export default Products;
+
+export async function getServerSideProps(context) {
+  const API_URL = process.env.API_URL;
+  const { locale } = context;
+
+  //Fetch API Header menu
+  let translationHeader = undefined;
+  const HeaderRes = await fetch(`${API_URL}/headers`);
+  const header = await HeaderRes.json();
+  if (locale === "en") {
+    const translationHeaderRes = await fetch(
+      `${API_URL}/headers?_locale=` + locale
+    );
+    translationHeader = await translationHeaderRes.json();
+  }
+
+  //Fetch API Products
+  let translationProducts = undefined;
+  const productsRes = await fetch(`${API_URL}/products`);
+  const product = await productsRes.json();
+  if (locale === "en") {
+    const translationHeaderRes = await fetch(
+      `${API_URL}/products?_locale=` + locale
+    );
+    translationProducts = await translationHeaderRes.json();
+  }
+
+  //Fetch API Footer
+  let translationFooter = undefined;
+  const footerRes = await fetch(`${API_URL}/footers`);
+  const footer = await footerRes.json();
+  if (locale === "en") {
+    const translationFooterRes = await fetch(
+      `${API_URL}/footers?_locale=` + locale
+    );
+    translationFooter = await translationFooterRes.json();
+  }
+
+  return {
+    props: {
+      header: translationHeader ? translationHeader : header,
+      product: translationProducts ? translationProducts : product,
+      footer: translationFooter ? translationFooter : footer,
+    },
+  };
+}
